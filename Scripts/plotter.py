61ba356d02ddf0120067bf70871ff7a0dc8bd35a
Dan Grenell <dgrenell@newdestinyhousing.org>
2022-08-22 14:41:47 -0400

    Removed line width from bar chart

--- Scripts/plotter.py
+++ Scripts/plotter.py
@@ -7,7 +7,7 @@ import plotly.express as px
 
 # %% IMPORT DATA
 # path = '../sample_data.csv'
-path = '../Data/Five System Total.csv'
+path = '../Data/Open Data Five System Total.csv'
 
 df = pd.read_csv(path)
 
@@ -66,6 +66,8 @@ fig = px.bar(
     width=800, height=400 # Using this for comparison with altair
 )
 
+fig.update_traces(marker_line_width=0)
+
 fig.update_layout(
     {
         'plot_bgcolor': 'rgba(0, 0, 0, 0)',
@@ -78,56 +80,58 @@ fig.update_layout(
 fig.write_html('plotly_chart.html')
 
 
-# %% ALTAIR
+# # %% ALTAIR
 
-df_alt = df.melt(
-    id_vars='data_period',
-    var_name='Shelter System',
-    value_name='Unique Count'
-    )
+# df_alt = df.melt(
+#     id_vars='data_period',
+#     var_name='Shelter System',
+#     value_name='Unique Count'
+#     )
 
-df_alt['data_period'] = df_alt['data_period'].astype('datetime64')
+# df_alt['data_period'] = df_alt['data_period'].astype('datetime64')
 
-df_alt['tooltip'] = df_alt.apply(lambda x: f"{x['Unique Count']} ({x['Shelter System']})", axis=1)
+# df_alt['tooltip'] = df_alt.apply(lambda x: f"{x['Unique Count']} ({x['Shelter System']})", axis=1)
 
-sort_order_map = {
-    'DHS Total Unique Count': 1,
-    'HRA DV Total Unique Count': 3,
-    'HRA HASA Total Unique Count': 2,
-    'DYCD Total Unique Count': 5,
-    'HPD Total Unique Count (105% Est.)': 4,
-}
+# sort_order_map = {
+#     'DHS Total Unique Count': 1,
+#     'HRA DV Total Unique Count': 3,
+#     'HRA HASA Total Unique Count': 2,
+#     'DYCD Total Unique Count': 5,
+#     'HPD Total Unique Count (105% Est.)': 4,
+# }
 
-df_alt['sort_order'] = df_alt.apply(lambda x: sort_order_map[x['Shelter System']], axis=1)
+# df_alt['sort_order'] = df_alt.apply(lambda x: sort_order_map[x['Shelter System']], axis=1)
 
-color_map = {
-    'DHS Total Unique Count': '#5cb8b2',
-    'HRA DV Total Unique Count': '#F99D1B',
-    'HRA HASA Total Unique Count': '#3fb0d9',
-    'DYCD Total Unique Count': '#1d7595',
-    'HPD Total Unique Count (105% Est.)': '#6ac2e2',
-}
+# color_map = {
+#     'DHS Total Unique Count': '#5cb8b2',
+#     'HRA DV Total Unique Count': '#F99D1B',
+#     'HRA HASA Total Unique Count': '#3fb0d9',
+#     'DYCD Total Unique Count': '#1d7595',
+#     'HPD Total Unique Count (105% Est.)': '#6ac2e2',
+# }
 
-df_alt['color'] = df_alt.apply(lambda x: color_map[x['Shelter System']], axis=1)
+# df_alt['color'] = df_alt.apply(lambda x: color_map[x['Shelter System']], axis=1)
 
-chart = alt.Chart(df_alt).mark_bar().encode(
-    x=alt.X('data_period', axis=alt.Axis(title='Data Period')),
-    y=alt.Y('sum(Unique Count)', axis=alt.Axis(title='Unique Count')),
-    # color='color',
-    tooltip='tooltip',
-    order=alt.Order(
-        'sort_order',
-        sort='ascending'
-    ),
-    color=alt.Color('color', scale=None, title='Shelter System')
-).configure_axis(
-    grid=False
-).configure_view(
-    strokeWidth=0
-)
+# chart = alt.Chart(df_alt).mark_bar().encode(
+#     x=alt.X('data_period', axis=alt.Axis(title='Data Period')),
+#     y=alt.Y('sum(Unique Count)', axis=alt.Axis(title='Unique Count')),
+#     # color='color',
+#     tooltip='tooltip',
+#     order=alt.Order(
+#         'sort_order',
+#         sort='ascending'
+#     ),
+#     color=alt.Color('color', scale=None, title='Shelter System')
+# ).configure_axis(
+#     grid=False
+# ).configure_view(
+#     strokeWidth=0
+# )
 
 
 
-chart.save('altair_chart.html')
+# chart.save('altair_chart.html')
+
+# # %%
 
 # %%
